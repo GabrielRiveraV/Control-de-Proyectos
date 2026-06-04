562bb156f84a58499d49682714c7dc171f76dbc0
Gabriel Rivera <gabrielrivv8@gmail.com>
2026-06-04 10:28:25 -0600

    V0.5 Expediente de Contrato añadido con historial de visitas. Versión estable del 04/06/2026 10:30 am

--- routes/proyectos.py
+++ routes/proyectos.py
@@ -2,7 +2,9 @@ from flask import (
     Blueprint,
     render_template,
     request,
-    redirect)
+    redirect,
+    url_for
+    )
 from utils.auditoria import registrar_auditoria
 from flask_login import (
     login_required,
@@ -25,8 +27,99 @@ def inicio():
 
     conexion = conectar_db()
     cursor = conexion.cursor(dictionary=True)
+    
+    if request.method == 'POST':
 
+        cursor.execute("""
+            UPDATE contratos
+            SET
+
+                no_contrato = %s,
+                fecha_contrato = %s,
+                contratista = %s,
+
+                inversion_autorizada = %s,
+                monto_contratado = %s,
+                anticipo = %s,
+
+                periodo_ejecucion = %s,
+                plazo_ejecucion = %s,
+
+                fecha_inicio_real = %s,
+                fecha_terminacion_real = %s,
+
+                convenio_diferimiento = %s,
+                convenio_suspension = %s,
+
+                monto_ejercido = %s,
+                saldo = %s,
+
+                residente_obra = %s,
+
+                documentacion_relacionada = %s,
+                informacion_auditorias = %s
+
+            WHERE id_contrato = %s
+        """, (
+
+            request.form.get('no_contrato'),
+            request.form.get('fecha_contrato'),
+            request.form.get('contratista'),
+
+            request.form.get('inversion_autorizada'),
+            request.form.get('monto_contratado'),
+            request.form.get('anticipo'),
+
+            request.form.get('periodo_ejecucion'),
+            request.form.get('plazo_ejecucion'),
+
+            request.form.get('fecha_inicio_real'),
+            request.form.get('fecha_terminacion_real'),
+
+            request.form.get('convenio_diferimiento'),
+            request.form.get('convenio_suspension'),
+
+            request.form.get('monto_ejercido'),
+            request.form.get('saldo'),
+
+            request.form.get('residente_obra'),
+
+            request.form.get('documentacion_relacionada'),
+            request.form.get('informacion_auditorias'),
+
+            id_contrato
+        ))
+
+        conexion.commit()
+    
     busqueda = request.args.get('busqueda')
+    pagina = request.args.get('pagina', 1, type=int)
+
+    por_pagina = 15
+
+    offset = (pagina - 1) * por_pagina
+
+    if busqueda:
+
+        cursor.execute("""
+            SELECT COUNT(*) AS total
+            FROM proyectos
+            WHERE nombre LIKE %s
+            OR programa LIKE %s
+        """,
+        (
+            f"%{busqueda}%",
+            f"%{busqueda}%"
+        ))
+
+    else:
+
+        cursor.execute("""
+            SELECT COUNT(*) AS total
+            FROM proyectos
+        """)
+    
+    total_registros = cursor.fetchone()['total']
 
     if busqueda:
 
@@ -77,66 +170,81 @@ def inicio():
             
 
         ORDER BY p.ejercicio_fiscal DESC,
-                 p.id_proyecto DESC
+                p.id_proyecto DESC
+
+        LIMIT %s, %s
         """
 
         cursor.execute(
             query,
             (
                 f"%{busqueda}%",
-                f"%{busqueda}%"
+                f"%{busqueda}%",
+            offset,
+            por_pagina
             )
         )
 
     else:
 
         cursor.execute("""
-                SELECT 
-            p.id_proyecto, 
-            p.nombre, 
-            p.programa, 
-            p.un_ad, 
-            p.localidad, 
-            p.inversion_autorizada,
-            p.ejercicio_fiscal,
+            SELECT
+                p.id_proyecto,
+                p.nombre,
+                p.programa,
+                p.un_ad,
+                p.localidad,
+                p.inversion_autorizada,
+                p.ejercicio_fiscal,
 
-            c.no_contrato,
+                c.no_contrato,
 
-            COUNT(v.id_visita) as total_visitas,
+                COUNT(v.id_visita) as total_visitas,
 
-            CASE
-                WHEN c.id_contrato IS NOT NULL THEN 1
-                ELSE 0
-            END as tiene_contrato,
+                CASE
+                    WHEN c.id_contrato IS NOT NULL THEN 1
+                    ELSE 0
+                END as tiene_contrato,
 
-            c.archivo_contrato,
-            c.id_contrato
+                c.archivo_contrato,
+                c.id_contrato
 
-        FROM proyectos p
+            FROM proyectos p
 
-        LEFT JOIN contratos c 
-            ON p.id_proyecto = c.id_proyecto
+            LEFT JOIN contratos c
+                ON p.id_proyecto = c.id_proyecto
 
-        LEFT JOIN visitas v 
-            ON c.id_contrato = v.id_contrato
+            LEFT JOIN visitas v
+                ON c.id_contrato = v.id_contrato
 
-        GROUP BY 
-            p.id_proyecto,
-            p.nombre,
-            p.programa,
-            p.un_ad,
-            p.localidad,
-            p.inversion_autorizada,
-            p.ejercicio_fiscal,
-            c.no_contrato,
-            c.id_contrato,
-            c.archivo_contrato
+            GROUP BY
+                p.id_proyecto,
+                p.nombre,
+                p.programa,
+                p.un_ad,
+                p.localidad,
+                p.inversion_autorizada,
+                p.ejercicio_fiscal,
+                c.no_contrato,
+                c.id_contrato,
+                c.archivo_contrato
 
-        ORDER BY p.ejercicio_fiscal DESC,
-                 p.id_proyecto DESC
-        """)
+            ORDER BY p.ejercicio_fiscal DESC,
+                    p.id_proyecto DESC
+
+            LIMIT %s, %s
+        """, (
+    offset,
+    por_pagina
+))
 
     proyectos = cursor.fetchall()
+    
+    import math
+
+    total_paginas = math.ceil(
+        total_registros / por_pagina
+    )
 
     # 🔹 MÉTRICAS
     cursor.execute("SELECT COUNT(*) AS total FROM proyectos")
@@ -182,6 +290,11 @@ def inicio():
     return render_template(
         "index.html",
         proyectos=proyectos,
+        
+        pagina=pagina,
+        total_paginas=total_paginas,
+        busqueda=busqueda,
+        
         total_proyectos=total_proyectos,
         total_contratos=total_contratos,
         total_visitas=total_visitas,
@@ -374,4 +487,141 @@ def auditoria():
     return render_template(
         'auditoria.html',
         registros=registros
+    )
+
+# ------------------------
+# RUTA PARA EL EXPEDIENTE DE LOS CONTRATOS
+# ------------------------
+#Función auxiliar
+def decimal_o_none(valor):
+    if valor == '':
+        return None
+    return valor
+#-------------------------
+@proyectos_bp.route(
+    '/expediente_contrato/<int:id_contrato>',
+    methods=['GET', 'POST']
+)
+@login_required
+def expediente_contrato(id_contrato):
+
+    conexion = conectar_db()
+    cursor = conexion.cursor(dictionary=True)
+
+    if request.method == 'POST':
+
+        no_contrato = request.form.get('no_contrato')
+        contratista = request.form.get('contratista')
+        fecha_contrato = request.form.get('fecha_contrato')
+
+        inversion_autorizada = decimal_o_none(request.form.get('inversion_autorizada'))
+        monto_contratado = decimal_o_none(request.form.get('monto_contratado'))
+        anticipo = decimal_o_none(request.form.get('anticipo'))
+        
+        periodo_ejecucion = request.form.get('periodo_ejecucion') or None
+        plazo_ejecucion = request.form.get('plazo_ejecucion') or None
+        fecha_inicio_real = request.form.get('fecha_inicio_real') or None
+        fecha_terminacion_real = request.form.get('fecha_terminacion_real') or None
+        convenio_diferimiento = request.form.get('convenio_diferimiento') or None
+        convenio_suspension = request.form.get('convenio_suspension') or None
+        monto_ejercido = request.form.get('monto_ejercido') or None
+        saldo = request.form.get('saldo') or None
+        residente_obra = request.form.get('residente_obra') or None
+        documentacion_relacionada = request.form.get('documentacion_relacionada') or None
+        informacion_auditorias = request.form.get('informacion_auditorias') or None
+
+        cursor.execute("""
+            UPDATE contratos
+            SET
+                no_contrato = %s,
+                fecha_contrato = %s,
+                contratista = %s,
+                inversion_autorizada = %s,
+                monto_contratado = %s,
+                anticipo = %s,
+                periodo_ejecucion = %s,
+                plazo_ejecucion = %s,
+                fecha_inicio_real = %s,
+                fecha_terminacion_real = %s,
+                convenio_diferimiento = %s,
+                convenio_suspension = %s,
+                monto_ejercido = %s,
+                saldo = %s,
+                residente_obra = %s,
+                documentacion_relacionada = %s,
+                informacion_auditorias = %s
+            WHERE id_contrato = %s
+        """, (
+
+            no_contrato,
+            fecha_contrato,
+            contratista,
+            inversion_autorizada,
+            monto_contratado,
+            anticipo,
+            periodo_ejecucion,
+            plazo_ejecucion,
+            fecha_inicio_real,
+            fecha_terminacion_real,
+            convenio_diferimiento,
+            convenio_suspension,
+            monto_ejercido,
+            saldo,
+            residente_obra,
+            documentacion_relacionada,
+            informacion_auditorias,
+            id_contrato))
+        
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+        return redirect(
+            url_for(
+                'proyectos.expediente_contrato',
+                id_contrato=id_contrato
+            )
+        )
+
+    cursor.execute("""
+        SELECT
+            c.*,
+
+            p.nombre,
+            p.programa,
+            p.un_ad,
+            p.localidad,
+            p.ejercicio_fiscal
+
+        FROM contratos c
+
+        INNER JOIN proyectos p
+            ON c.id_proyecto = p.id_proyecto
+
+        WHERE c.id_contrato = %s
+    """, (id_contrato,))
+
+    contrato = cursor.fetchone()
+    
+    cursor.execute("""
+        SELECT
+            id_visita,
+            fecha_visita,
+            supervisor,
+            residente_obra,
+            observaciones,
+            archivo_acta
+        FROM visitas
+        WHERE id_contrato = %s
+        ORDER BY fecha_visita DESC
+    """, (id_contrato,))
+
+    visitas = cursor.fetchall()
+
+    conexion.close()
+
+    return render_template(
+        "expediente_contrato.html",
+        contrato=contrato,
+        visitas=visitas
     )
